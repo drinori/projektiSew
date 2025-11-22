81543d52483299a0d93953d6a9e8ebc076292753
LatifKovani <latif.kovani.st@uni.gjilan.net>
2025-11-22 14:29:57 +0100

    designed kycja.jsx and regjistrimi.jsx

--- frontend/src/components/Kycja.jsx
+++ frontend/src/components/Kycja.jsx
@@ -25,44 +25,57 @@ function Kycja() {
 
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <div>
-        <p className="font-bold py-10 text-2xl">Kycu</p>
-        <form onSubmit={validateForm}>
-          <div>
-            <label htmlFor="email" className="block">
-              Email
-            </label>
-            <input
-              type="text"
-              placeholder="Email"
-              className="border"
-              onChange={(e) => setData({ ...data, email: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="fjalekalimi">Fjalekalimi</label>
-            <div>
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Fjalekalimi"
-                className="border"
-                onChange={(e) => setData({ ...data, password: e.target.value })}
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute px-2"
-              >
-                {showPassword ? "Fshih" : "Shfaq"}
-              </button>
-            </div>
-          </div>
-          <button type="submit" className="block">
+      <div
+        class="flex justify-center items-center w-full sm:w-[300px] md:w-[400px] lg:w-[500px]
+              h-[300px] sm:h-[400px] md:h-[450px] lg:h-[500px]
+              bg-white rounded-lg shadow-2xl p-8 sm:p-10 md:p-12"
+      >
+        <div>
+          <h1 className="font-bold text-4xl flex justify-center items-center mb-2 sm:mb-6 md:mb-9">
             Kycu
-          </button>
-          <p className="inline">Nuk keni llogari?</p>
-          <Link to="/regjistrimi">Regjistrohu</Link>
-        </form>
+          </h1>
+          <form onSubmit={validateForm} className="space-y-5">
+            <div>
+              <label htmlFor="email" className="block">
+                Email
+              </label>
+              <input
+                type="text"
+                placeholder="Email"
+                className="border rounded-sm p-1 w-full sm:w-80 md:w-95 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+                onChange={(e) => setData({ ...data, email: e.target.value })}
+              />
+            </div>
+            <div>
+              <label htmlFor="fjalekalimi">Fjalekalimi</label>
+              <div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Fjalekalimi"
+                  className="border rounded-sm p-1 w-full sm:w-80 md:w-95 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+                  onChange={(e) =>
+                    setData({ ...data, password: e.target.value })
+                  }
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute px-2"
+                >
+                  {showPassword ? "Fshih" : "Shfaq"}
+                </button>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="block border rounded-sm p-1 w-full sm:w-80 md:w-95 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+            >
+              Kycu
+            </button>
+            <p className="inline">Nuk keni llogari?</p>
+            <Link to="/regjistrimi">Regjistrohu</Link>
+          </form>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Regjistrimi.jsx
+++ frontend/src/components/Regjistrimi.jsx
@@ -46,103 +46,126 @@ function Regjistrimi() {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div>
-        <h1 className="font-bold text-2xl">Regjistrimi</h1>
-        <form onSubmit={validateForm}>
-          <div>
-            <label htmlFor="aplikant">Aplikant</label>
-            <input
-              type="radio"
-              name="tipiPerdoruesit"
-              value="aplikant"
-              className="border"
-              required
-              onChange={(e) =>
-                setData({ ...data, tipiPerdoruesit: e.target.value })
+      <div
+        class="flex justify-center items-center w-full sm:w-[500px] md:w-[600px] lg:w-[650px]
+                      transition-all duration-300 ease-in-out
+                      bg-white rounded-lg shadow-2xl p-6 sm:p-8 md:p-10"
+      >
+        <div>
+          <h1 className="font-bold text-4xl flex justify-center items-center mb-2 sm:mb-6 md:mb-9">
+            Regjistrimi
+          </h1>
+          <form onSubmit={validateForm} className="space-y-3">
+            <div className="flex items-center justify-center gap-4 sm:gap-6 md:gap-8 mb-4 sm:mb-6 text-xl">
+              <label
+                htmlFor="aplikant"
+                className="flex items-center gap-1 sm:gap-2 cursor-pointer"
+              >
+                <span className="text-sm sm:text-xl ">Aplikant</span>
+                <input
+                  type="radio"
+                  name="tipiPerdoruesit"
+                  value="aplikant"
+                  className="border w-4 h-4 cursor-pointer"
+                  required
+                  onChange={(e) =>
+                    setData({ ...data, tipiPerdoruesit: e.target.value })
+                  }
+                />
+              </label>
+              <label
+                htmlFor="punedhenes"
+                className="flex items-center gap-1 sm:gap-2 cursor-pointer"
+              >
+                <span className="text-sm sm:text-xl ">Punedhenes</span>
+                <input
+                  type="radio"
+                  name="tipiPerdoruesit"
+                  value="punedhenes"
+                  className="border w-4 h-4 cursor-pointer"
+                  required
+                  onChange={(e) =>
+                    setData({ ...data, tipiPerdoruesit: e.target.value })
+                  }
+                />
+              </label>
+            </div>
+            <div
+              className={
+                data.tipiPerdoruesit === "punedhenes" ? "hidden" : "block"
               }
-            />
-            <label htmlFor="punedhenes">Punedhenes</label>
-            <input
-              type="radio"
-              name="tipiPerdoruesit"
-              value="punedhenes"
-              className="border"
-              required
-              onChange={(e) =>
-                setData({ ...data, tipiPerdoruesit: e.target.value })
+            >
+              <label htmlFor="emri">Emri</label>
+              <input
+                type="text"
+                className="border block rounded-sm p-1 w-full sm:w-80 md:w-96 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+                value={data.emri}
+                onChange={(e) => setData({ ...data, emri: e.target.value })}
+              />
+            </div>
+            <div
+              className={
+                data.tipiPerdoruesit === "punedhenes" ? "hidden" : "block"
               }
-            />
-          </div>
-          <div
-            className={
-              data.tipiPerdoruesit === "punedhenes" ? "hidden" : "block"
-            }
-          >
-            <label htmlFor="emri">Emri</label>
-            <input
-              type="text"
-              className="border"
-              value={data.emri}
-              onChange={(e) => setData({ ...data, emri: e.target.value })}
-            />
-          </div>
-          <div
-            className={
-              data.tipiPerdoruesit === "punedhenes" ? "hidden" : "block"
-            }
-          >
-            <label htmlFor="mbiemri">Mbiemri</label>
-            <input
-              type="text"
-              className="border"
-              onChange={(e) => setData({ ...data, mbiemri: e.target.value })}
-            />
-          </div>
-          <div
-            className={
-              data.tipiPerdoruesit === "punedhenes" ? "block" : "hidden"
-            }
-          >
-            <label htmlFor="kompania">Kompania</label>
-            <input
-              type="text"
-              className="border"
-              value={data.kompania}
-              onChange={(e) => setData({ ...data, kompania: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              className="border"
-              onChange={(e) => setData({ ...data, email: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="fjalekalimi">Fjalekalimi</label>
-            <input
-              type="text"
-              className="border"
-              onChange={(e) =>
-                setData({ ...data, fjalekalimi: e.target.value })
+            >
+              <label htmlFor="mbiemri">Mbiemri</label>
+              <input
+                type="text"
+                className="border block rounded-sm p-1 w-full sm:w-80 md:w-96 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+                onChange={(e) => setData({ ...data, mbiemri: e.target.value })}
+              />
+            </div>
+            <div
+              className={
+                data.tipiPerdoruesit === "punedhenes" ? "block" : "hidden"
               }
-            />
-          </div>
-          <div>
-            <label htmlFor="konfirmoFjalekalimin">Konfirmo Fjalekalimin</label>
-            <input
-              type="text"
-              className="border"
-              onChange={(e) =>
-                setData({ ...data, konfirmoFjalekalimin: e.target.value })
-              }
-            />
-          </div>
-          <button type="submit" className="border ">
-            Regjistrohu
-          </button>
-        </form>
+            >
+              <label htmlFor="kompania">Kompania</label>
+              <input
+                type="text"
+                className="border block rounded-sm p-1 w-full sm:w-80 md:w-96 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+                value={data.kompania}
+                onChange={(e) => setData({ ...data, kompania: e.target.value })}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="border block rounded-sm p-1 w-full sm:w-80 md:w-96 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+                onChange={(e) => setData({ ...data, email: e.target.value })}
+              />
+            </div>
+            <div>
+              <label htmlFor="fjalekalimi">Fjalekalimi</label>
+              <input
+                type="text"
+                className="border block rounded-sm p-1 w-full sm:w-80 md:w-96 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+                onChange={(e) =>
+                  setData({ ...data, fjalekalimi: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label htmlFor="konfirmoFjalekalimin">
+                Konfirmo Fjalekalimin
+              </label>
+              <input
+                type="text"
+                className="border block rounded-sm p-1 w-full sm:w-80 md:w-96 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+                onChange={(e) =>
+                  setData({ ...data, konfirmoFjalekalimin: e.target.value })
+                }
+              />
+            </div>
+            <button
+              type="submit"
+              className="border block rounded-sm p-1 w-full sm:w-80 md:w-96 lg:w-[350px] h-10 sm:h-12 md:h-14 lg:h-10"
+            >
+              Regjistrohu
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
