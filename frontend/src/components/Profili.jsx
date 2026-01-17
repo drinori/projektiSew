f92fda2ffd9401b9768d3a175ccf9c154135c440
LatifKovani <151386211+LatifKovani@users.noreply.github.com>
2026-01-17 11:04:05 +0100

    Designed Profili

--- frontend/src/components/Profili.jsx
+++ frontend/src/components/Profili.jsx
@@ -3,7 +3,15 @@ import "../index.css";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import Header from "./Header";
-import { Mail, Phone, Briefcase, Plus } from "lucide-react";
+import {
+  Mail,
+  Phone,
+  Briefcase,
+  Plus,
+  GraduationCap,
+  FolderKanban,
+  Edit2,
+} from "lucide-react";
 
 function Profili() {
   const navigate = useNavigate();
@@ -59,7 +67,7 @@ function Profili() {
     <div className="min-h-screen bg-gray-100">
       <Header perdoruesiData={perdoruesiData} onCkycja={handleCkycja} />
 
-      <div className="max-w-4xl mx-auto m-10">
+      <div className="max-w-4xl mx-auto mb-2 mt-10">
         <div className="bg-white rounded-2xl shadow-lg overflow-hidden">
           <div className="h-32 bg-white/30 "></div>
 
@@ -94,10 +102,12 @@ function Profili() {
           <div className="bg-white rounded-2xl shadow-lg overflow-hidden p-8">
             <div className="flex items-center justify-between mb-6">
               <div className="flex items-center gap-2">
-                <Briefcase className="text-blue-600" size={24} />
-                <h2 className="text-2xl font-bold text-gray-900">Experience</h2>
+                <Briefcase size={24} />
+                <h2 className="text-2xl font-bold text-gray-900">
+                  Eksperienca
+                </h2>
               </div>
-              <button className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
+              <button className="publikoPune flex items-center gap-1">
                 <Plus size={18} />
                 Shto
               </button>
@@ -105,29 +115,119 @@ function Profili() {
             <div className="bg-gray-50 p-4 rounded-lg mb-4 space-y-3">
               <input
                 type="text"
-                placeholder="Job Title"
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Titulli i pozicionit"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               />
               <input
                 type="text"
-                placeholder="Company"
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Kompania"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               />
-              <input
-                type="text"
-                placeholder="Period (e.g., 2020 - Present)"
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
+              <div className="grid grid-cols-2 gap-3">
+                <input
+                  type="text"
+                  placeholder="Data fillimit"
+                  className="px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+                <input
+                  type="text"
+                  placeholder="Data mbarimit"
+                  className="px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+              </div>
               <textarea
-                placeholder="Description"
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Përshkrimi"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 rows="3"
               />
               <div className="flex gap-2">
-                <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
-                  Ruaj
+                <button className="publikoPune">Ruaj</button>
+                <button className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300">
+                  Anulo
                 </button>
-                <button className="px-4 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 transition-colors">
+              </div>
+            </div>
+            <div className="flex items-center justify-between mb-6 mt-10">
+              <div className="flex items-center gap-2 ">
+                <GraduationCap size={24} />
+                <h2 className="text-2xl font-bold text-gray-900  ">Edukimi</h2>
+              </div>
+              <button className="publikoPune flex items-center gap-1">
+                <Plus size={18} />
+                Shto
+              </button>
+            </div>
+            <div className="bg-gray-50 p-4 rounded-lg mb-4 space-y-3">
+              <input
+                type="text"
+                placeholder="Diploma/Titulli"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+              <input
+                type="text"
+                placeholder="Institucioni"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+              <div className="grid grid-cols-2 gap-3">
+                <input
+                  type="date"
+                  placeholder="Data fillimit"
+                  className="px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+                <input
+                  type="date"
+                  placeholder="Data mbarimit"
+                  className="px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+              </div>
+              <textarea
+                placeholder="Përshkrimi"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                rows="3"
+              />
+              <div className="flex gap-2">
+                <button className="publikoPune">Ruaj</button>
+                <button className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300">
+                  Anulo
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center justify-between mb-6 mt-10">
+              <div className="flex items-center gap-2 ">
+                <FolderKanban size={24} />
+                <h2 className="text-2xl font-bold text-gray-900  ">
+                  Projektet
+                </h2>
+              </div>
+              <button className="publikoPune flex items-center gap-1">
+                <Plus size={18} />
+                Shto
+              </button>
+            </div>
+            <div className="bg-gray-50 p-4 rounded-lg mb-4 space-y-3">
+              <input
+                type="text"
+                placeholder="Emri i projektit"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+              <textarea
+                placeholder="Përshkrimi"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                rows="3"
+              />
+              <input
+                type="text"
+                placeholder="Teknologjitë (p.sh. React, Node.js, MongoDB)"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+              <input
+                type="text"
+                placeholder="Link (opsionale)"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+              <div className="flex gap-2">
+                <button className="publikoPune">Ruaj</button>
+                <button className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300">
                   Anulo
                 </button>
               </div>
